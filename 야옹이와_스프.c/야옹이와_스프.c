947e24e5f165f8c66118d5c33621c35e43b14c43
zxcxz8790 <zxcxz8790@gmail.com>
2025-06-02 23:22:56 +0900

    2-1 CP, 기분 출력 가구 위치 랜덤 설정 함수 생성 가구 출력 안함(이후 반영 예정)

--- 야옹이와_스프.c/야옹이와_스프.c
+++ 야옹이와_스프.c/야옹이와_스프.c
@@ -6,13 +6,15 @@
 
 #define ROOM_WIDTH 10
 #define HME_POS 1
-#define BWL_PO (ROOM_WIDTH - 2)
+#define BWL_POS (ROOM_WIDTH - 2)
 
-void printStatus(int soupCount, int level);//상태출력
+void printStatus(int soupCount, int level, int cp, char name[], int mood);//상태출력
 void interaction(char name[], int* level);//상호작용
 void room(int cat,int previous);//방그리기
 void moveCat(char name[], int* cat, int level);//이동
 void action(char name[], int cat, int* soup);//행동
+
+int towerPos = -1, scratcherPos = -1; //2-1 방그리기 현재 위치 미정
 
 int main(void) {
 	srand((unsigned int)time(NULL));
@@ -39,8 +41,9 @@ int main(void) {
 
 	int soup = 0, level = 2;
 	int cat = 1, previous = 0;
+	int mood = 3, cp = 0;
 
-	printStatus(soup, level);
+	printStatus(soup, level, cp, name, mood);
 	room(cat, previous);
 
 	while (1) {
@@ -52,15 +55,15 @@ int main(void) {
 
 		Sleep(2500);
 		system("cls");
-		printStatus(soup, level);
+		printStatus(soup, level, cp, name, mood);
 		room(cat, previous);
 	}
 
 	return 0;
 }
 
-void printStatus(int soupCount, int level) {
-	const char* messages[] = {
+void printStatus(int soupCount, int level, int cp, char name[], int mood) {//2-1 상태창 변경, CP,기분 출력
+	const char* levelMessages[] = {
 		"0: 곁에 오는 것도 싫어합니다.",
 		"1: 간식 자판기 취급입니다.",
 		"2: 그럭저럭 쓸 만한 집사입니다.",
@@ -68,10 +71,20 @@ void printStatus(int soupCount, int level) {
 		"4: 집사 껌딱지입니다."
 	};
 
+	const char* moodMessages[] = {
+		"0: 기분이 매우 나쁩니다.",
+		"1: 심심해집니다.",
+		"2: 식빵을 굽습니다.",
+		"3: 골골송을 부릅니다."
+	};
+
 	printf("=============== 현재 상태 ===============\n");
 	printf("현재까지 만든 수프: %d개\n", soupCount);
+	printf("CP : %d 포인트\n", cp);
+	printf("%s 의 기분 (0~3) : %d\n", name, mood);
+	printf("\t%s\n",moodMessages[mood]);
 	printf("집사와의 관계(0~4): %d\n", level);
-	printf("%s\n", messages[level]);
+	printf("%s\n", levelMessages[level]);
 	printf("=========================================\n");
 	
 	Sleep(500);
@@ -124,6 +137,8 @@ void interaction(char name[], int* level) {
 }
 
 void room(int cat, int previous) {
+	int tower = 0, scratcher = 0; //2-1 방그리기 가구 놀이기구 미구매 상태
+
 	for (int i = 0; i < ROOM_WIDTH; i++) {
 		printf("#");
 	}
@@ -132,7 +147,9 @@ void room(int cat, int previous) {
 	printf("#");
 	for (int i = 1; i < ROOM_WIDTH-1; i++) {
 		if (i == HME_POS) printf("H");
-		else if (i == BWL_PO) printf("B");
+		else if (i == BWL_POS) printf("B");
+		else if (tower && i == towerPos) printf("T"); //캣타워 출력, 현재는 미반영
+		else if (scratcher && i == scratcherPos) printf("S");// 스크래쳐 출력, 현재는 미반영
 		else printf(" ");
 	}
 	printf("#\n");
@@ -187,7 +204,7 @@ void action(char name[], int cat, int* soup) {
 	if (cat == HME_POS) {
 		printf("%s(은)는 자신의 집에서 편안함을 느낍니다.\n", name);
 	}
-	else if (cat == BWL_PO) {
+	else if (cat == BWL_POS) {
 		int soupnum = rand() % 3;
 
 		printf("%s(이)가 ", name);
@@ -205,4 +222,14 @@ void action(char name[], int cat, int* soup) {
 	}
 
 	Sleep(500);
+}
+
+void FurniturePosition() {//2-1 방그리기 위치 무작위 설정
+	do {
+		towerPos = rand() % (ROOM_WIDTH - 2) + 1;
+	} while (towerPos == HME_POS || towerPos == BWL_POS);
+
+	do {
+		scratcherPos = rand() % (ROOM_WIDTH - 2) + 1;
+	} while (scratcherPos == HME_POS || scratcherPos == BWL_POS || scratcherPos == towerPos);
 }
